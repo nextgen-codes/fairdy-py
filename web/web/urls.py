036503019b3c8ae0c1c9b5e8a1e6816f11842760
ajb240838 <aj.becker@stud.uis.no>
2020-05-24 22:50:30 +0200

    deployment

--- web/web/urls.py
+++ web/web/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 from fairdy.views import *
@@ -23,3 +24,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('fairdy/', include('fairdy.urls')),
 ]
+urlpatterns += staticfiles_urlpatterns()
